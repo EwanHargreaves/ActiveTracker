efa2a0a3e00cc0ea7fe2d371d42ba7bccda53bc0
EwanHargreaves <ewanhargreaves2002@gmail.com>
2024-04-05 21:30:14 +0100

    Created Tkinter exapmle GUI https://tkdocs.com/tutorial/firstexample.html

---
+++ src/main.py
@@ -0,0 +1,22 @@
+def main():
+    pass
+
+    # create the GUI window
+    #   Has a start and stop button
+
+    # On start check state of cpu to see if still working
+    # If idle detected, start idle timer unil input is detected
+    # program shut itself down if idle for too long? (i.e. computer turns off)
+
+    #Once work session is ended record results
+    #   Total Time
+    #   Active Time
+    #   Idle Time
+    #   Idle Sessions
+    #    Idle Sessions count
+    #    Idle Sessions time average idle duration
+    #    Idle Sessions time of each idle start
+
+    # Show off activity data from past sessions in a visual way using the GUI
+
+main()
